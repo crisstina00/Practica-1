74c04acedcb5966e8f3751966f2c82dadc41f61d
admin <cristinamon06@gmail.com>
2024-11-17 20:39:27 +0100

    Creacion de build.sh y demas

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mi Primera página web</title>
+</head>
+<body>
+<h1>Mi primera página web</h1>
+<a href="index.html">Index.html</a>
+</body>
+</html>
+
